a79b78c71f9d22888b5e4e003cd643f40cc6a060
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2021-08-21 23:38:11 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+var number = 10;
+var lastFrame = 0;
+function numberGoesDown() {
+  if (number <= 25 && number >= 1) {
+    number--;
+  }
+}
+function nextFrame(timeStamp) {
+  let dt = timeStamp - lastFrame;
+  lastFrame = timeStamp;
+  number += dt/1000;
+  if (number > 25) {
+    document.getElementById("number").innerText = "You lose"
+  } else document.getElementById("number").innerText = number;
+}
+window.requestAnimationFrame(nextFrame)
